a78a0fef897cdab70e9a9c1736027d7a4a2ae63f
yuichi tanaka <u1tnk1123@gmail.com>
2011-06-20 22:08:54 +0900

    first arabica!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arabica::Application.config.secret_token = '7b679a5b1e0c05dc4d6ed0af87237208862dd64a876b8a5fed84c9df7687a4cbd4eb0b26767e7acf5479dbc1529d0809f944897be254ef62363e109e20896bb7'
