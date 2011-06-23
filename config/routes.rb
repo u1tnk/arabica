daa99c8d318bd592769edd3f9ad98547d6d341ab
yuichi tanaka <u1tnk1123@gmail.com>
2011-06-23 23:25:35 +0900

    /tlのみをルートに追加

--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,8 @@ Arabica::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-  get '/tl/:name' => "time_line#index", :except => ['/login']
+  get '/tl/:name' => "time_line#index"
+  #name指定無ければログインユーザ 
+  get '/tl' => "time_line#index"
 end
 
