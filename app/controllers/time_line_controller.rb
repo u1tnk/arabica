9b7c2bcf9433a784a29d482ca787ace88465348c
yuichi tanaka <u1tnk1123@gmail.com>
2011-07-04 23:11:33 +0900

    fix #44 urlの取得を素直にfind_by_sqlにした。Arel検討したが複雑怪奇になりそうなので…

--- app/controllers/time_line_controller.rb
+++ app/controllers/time_line_controller.rb
@@ -6,12 +6,27 @@ class TimeLineController < AuthorizedController
       # 初回アクセス時は現在のtime_lineから50件取り込む、最大200件可能だが、時間がかかるため
       unless current_user.tweets.exists?
         @client = twitter_client
-        raw_tweets = @client.home_timeline(:count => 200)
+        raw_tweets = @client.home_timeline(:count => 50)
         Url.filter_urls_from_tweets current_user, raw_tweets
       end
-
-      @user = current_user
-      @urls = @user.tweets.map{|t|t.urls}.flatten.uniq
     end
+    @user = current_user
+    sql = "
+      select distinct 
+        urls.*
+      from 
+        urls 
+        inner join tweets_urls on urls.id = tweets_urls.url_id 
+        inner join tweets_users on tweets_urls.tweet_id = tweets_users.tweet_id 
+        inner join users on users.id = tweets_users.user_id 
+      where
+        users.id = ?
+    ";
+    @urls = Url.find_by_sql [sql, @user.id]
+    return
+    @urls = Url.join("tweets_users").limit(5)
+    p "aa"
+    p @urls.size
+    p "aa"
   end
 end
